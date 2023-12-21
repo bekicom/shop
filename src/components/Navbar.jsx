7746e313845ffb0ca64012bd75f28b3e5718bcc9
bekicom <adelkauz6@gmail.com>
2023-12-21 21:11:38 +0500

    satrt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,10 @@ import { styled, alpha } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import Button from '@mui/material/Button';
-import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 
 export default function Navbar() {
